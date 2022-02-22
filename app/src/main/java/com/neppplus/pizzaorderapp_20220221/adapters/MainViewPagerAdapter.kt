65eec92ff813cc37e4b02dbcff926619e29ea070
이진실 <ljsil1204@naver.com>
2022-02-22 18:21:38 +0900

    도전과제3 - 치킨 주문 프래그먼트 추가 작업 중

--- app/src/main/java/com/neppplus/pizzaorderapp_20220221/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/neppplus/pizzaorderapp_20220221/adapters/MainViewPagerAdapter.kt
@@ -3,15 +3,17 @@ package com.neppplus.pizzaorderapp_20220221.adapters
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.neppplus.pizzaorderapp_20220221.fragments.ChickenFragment
 import com.neppplus.pizzaorderapp_20220221.fragments.ProfileFragment
 import com.neppplus.pizzaorderapp_20220221.fragments.PizzaFragment
 
 class MainViewPagerAdapter( fm : FragmentManager ) : FragmentPagerAdapter(fm) {
-    override fun getCount(): Int = 2
+    override fun getCount(): Int = 3
 
     override fun getItem(position: Int): Fragment {
         return when(position) {
             0 -> PizzaFragment()
+            1 -> ChickenFragment()
             else -> ProfileFragment()
         }
     }
@@ -19,6 +21,7 @@ class MainViewPagerAdapter( fm : FragmentManager ) : FragmentPagerAdapter(fm) {
     override fun getPageTitle(position: Int): CharSequence? {
         return when(position){
             0 -> "피자 주문"
+            1 -> "치킨 주문"
             else -> "내 정보 설정"
         }
     }
