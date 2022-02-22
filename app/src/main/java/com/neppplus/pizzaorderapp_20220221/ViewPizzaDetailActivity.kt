0840116dc3f77511f80dcb70272809f73a05f52f
이진실 <ljsil1204@naver.com>
2022-02-22 09:32:08 +0900

    전화 걸기 라이브러리 작업중

--- app/src/main/java/com/neppplus/pizzaorderapp_20220221/ViewPizzaDetailActivity.kt
+++ app/src/main/java/com/neppplus/pizzaorderapp_20220221/ViewPizzaDetailActivity.kt
@@ -1,8 +1,12 @@
 package com.neppplus.pizzaorderapp_20220221
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import com.neppplus.pizzaorderapp_20220221.datas.PizzaData
 import kotlinx.android.synthetic.main.activity_view_pizza_detail.*
 
@@ -16,6 +20,22 @@ class ViewPizzaDetailActivity : AppCompatActivity() {
         Glide.with(this).load(pizzaData.storeImgUri).into(imgStoreLogo)
         txtStoreName.text = pizzaData.storeName
 
+
+        val pl = object : PermissionListener{
+            override fun onPermissionGranted() {
+                val myUri = Uri.parse("tel:01033337777")
+                val myIntent = Intent( Intent.ACTION_CALL,  myUri )
+                startActivity( myIntent )
+            }
+
+            override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+
+            }
+        }
+
+//        TedPermission.create()
+//            .setPermissionListener(pl)
+//            .setPermissions()
 
     }
 }
